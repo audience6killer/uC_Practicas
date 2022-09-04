17ae0fd1f167d7a6b96586ddece2ad54a2553fc8
vill4in <j.adrian.pulido.s@gmail.com>
2022-09-03 22:27:38 -0500

    Primera practica (not_checked yet)

---
+++ Practica_1_Puertos_Operaciones.X/build/default/production/PRAC_1.o.d
@@ -0,0 +1,4 @@
+build/default/production/PRAC_1.o:  \
+	/home/vill4in/MPLABXProjects/Practicas_Escuela/Practica_1_Puertos_Operaciones.X/PRAC_1.asm  \
+	/home/vill4in/MPLABXProjects/Practicas_Escuela/ENCABEZADO.inc  \
+
